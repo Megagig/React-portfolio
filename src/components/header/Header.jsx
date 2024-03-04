fc7b42c7525eaafb830d9ed6f92c9ca682335152
Megagig <megagigsoftwaresolutions.com>
2024-03-04 16:53:00 +0100

    Add CTA and  HeaderSocials components with the Profile Image

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,8 +1,25 @@
 import React from 'react';
+import CTA from './CTA.jsx';
 import './header.css';
+import ME from '../../assets/me.png';
 
 const Header = () => {
-  return <h1>This is the header</h1>;
+  return (
+    <header>
+      <div className="container header__container">
+        <h5>Hello, I'm</h5>
+        <h1>Anthony Obi</h1>
+        <h5 className="text-light">Fullstack Developer</h5>
+        <CTA />
+        <div className="me">
+          <img src={ME} alt="profile" />
+          <a href="#contact" className="scroll__down">
+            Scroll Down
+          </a>
+        </div>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
---
+++ src/components/header/HeaderSocials.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const HeaderSocials = () => {
+  return (
+    <div className="header__socials">
+      <a href="https://www.linkedin.com/in/anthony-obi/">
+        <i className="fa fa-linkedin"></i>
+      </a>
+      <a href="https://github.com/anthonyobi">
+        <i className="fa fa-github"></i>
+      </a>
+      <a href="https://www.instagram.com/anthonyobi/">
+        <i className="fa fa-instagram"></i>
+      </a>
+      <a href="mailto:<EMAIL>">
+        <i className="fa fa-envelope"></i>
+      </a>
+    </div>
+  );
+};
+
+export default HeaderSocials;
