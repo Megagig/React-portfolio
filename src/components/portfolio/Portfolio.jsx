dc76ca2a87f253c628ef003669fce995299dc8c5
Megagig <megagigsoftwaresolutions.com>
2024-03-08 15:11:48 +0100

    Create a Dynamic data of arrays to be used for mao method

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -6,145 +6,58 @@ import CinemaApp from '../../assets/Cinema.png';
 import RemoteJob from '../../assets/RemoteJob.png';
 import SpaceTravellers from '../../assets/SpaceTravellers.png';
 import MathMagician from '../../assets/Math-Magicians.png';
+
+const data = [
+  {
+    id: 1,
+    image: Jefclarem,
+    title: 'Company website for Jefclarem Integrated Logistics LTD.',
+    Github: 'https://github.com/Megagig/Jefclarem_logistics',
+    demo: 'http://jefclarem-logistics.vercel.app/',
+  },
+  {
+    id: 2,
+    image: CalorieTracker,
+    title: 'All-in-one food tracker and health app ',
+    Github: 'https://github.com/Megagig/Calorie-Tracker',
+    demo: 'https://megagig-calorietracker.netlify.app/',
+  },
+  {
+    id: 3,
+    image: CinemaApp,
+    title: 'A Movie and Tv Web Application',
+    Github: 'https://github.com/Megagig/Cinema-Cafe-App',
+    demo: 'https://cinema-cafe-app.vercel.app/',
+  },
+  {
+    id: 4,
+    image: RemoteJob,
+    title: 'Remote Job App',
+    Github: 'https://github.com/Megagig/Remote-Job-App',
+    demo: 'https://remote-job-app.onrender.com/',
+  },
+  {
+    id: 5,
+    image: SpaceTravellers,
+    title: 'Space Travellers Hub',
+    Github: 'https://github.com/Megagig/space-travellers',
+    demo: 'https://space-travellers-1pm9.onrender.com/',
+  },
+  {
+    id: 6,
+    image: MathMagician,
+    title: 'The Math Magicians App',
+    Github: 'https://github.com/Megagig/math-magicians',
+    demo: 'https://math-magicians-8cw7.onrender.com/',
+  },
+];
+
 const Portfolio = () => {
   return (
     <section id="portfolio">
       <h5>My Recent Work</h5>
       <h2>Portfolio</h2>
-      <div className="container portfolio__container">
-        <article className="portfolio__item">
-          <div className="portfolio__item-image">
-            <img src={Jefclarem} alt="Anthony" />
-          </div>
-          <h3>This is a portfolio item heading</h3>
-          <div className="portfolio__item-cta">
-            <a
-              href="https://github.com/YOUR PROJECT URL"
-              className="btn"
-              target="_blank"
-            >
-              Github
-            </a>
-            <a
-              href="https://jefclarem-logistics.vercel.app/"
-              className="btn btn-primary"
-              target="_blank"
-            >
-              live Demo
-            </a>
-          </div>
-        </article>
-        <article className="portfolio__item">
-          <div className="portfolio__item-image">
-            <img src={CalorieTracker} alt="Anthony" />
-          </div>
-          <h3>This is a portfolio item heading</h3>
-          <div className="portfolio__item-cta">
-            <a
-              href="https://github.com/YOUR PROJECT URL"
-              className="btn"
-              target="_blank"
-            >
-              Github
-            </a>
-            <a
-              href="https://jefclarem-logistics.vercel.app/"
-              className="btn btn-primary"
-              target="_blank"
-            >
-              live Demo
-            </a>
-          </div>
-        </article>
-        <article className="portfolio__item">
-          <div className="portfolio__item-image">
-            <img src={CinemaApp} alt="Anthony" />
-          </div>
-          <h3>This is a portfolio item heading</h3>
-          <div className="portfolio__item-cta">
-            <a
-              href="https://github.com/YOUR PROJECT URL"
-              className="btn"
-              target="_blank"
-            >
-              Github
-            </a>
-            <a
-              href="https://jefclarem-logistics.vercel.app/"
-              className="btn btn-primary"
-              target="_blank"
-            >
-              live Demo
-            </a>
-          </div>
-        </article>
-        <article className="portfolio__item">
-          <div className="portfolio__item-image">
-            <img src={RemoteJob} alt="Anthony" />
-          </div>
-          <h3>This is a portfolio item heading</h3>
-          <div className="portfolio__item-cta">
-            <a
-              href="https://github.com/YOUR PROJECT URL"
-              className="btn"
-              target="_blank"
-            >
-              Github
-            </a>
-            <a
-              href="https://jefclarem-logistics.vercel.app/"
-              className="btn btn-primary"
-              target="_blank"
-            >
-              live Demo
-            </a>
-          </div>
-        </article>
-        <article className="portfolio__item">
-          <div className="portfolio__item-image">
-            <img src={SpaceTravellers} alt="Anthony" />
-          </div>
-          <h3>This is a portfolio item heading</h3>
-          <div className="portfolio__item-cta">
-            <a
-              href="https://github.com/YOUR PROJECT URL"
-              className="btn"
-              target="_blank"
-            >
-              Github
-            </a>
-            <a
-              href="https://jefclarem-logistics.vercel.app/"
-              className="btn btn-primary"
-              target="_blank"
-            >
-              live Demo
-            </a>
-          </div>
-        </article>
-        <article className="portfolio__item">
-          <div className="portfolio__item-image">
-            <img src={MathMagician} alt="Anthony" />
-          </div>
-          <h3>This is a portfolio item heading</h3>
-          <div className="portfolio__item-cta">
-            <a
-              href="https://github.com/YOUR PROJECT URL"
-              className="btn"
-              target="_blank"
-            >
-              Github
-            </a>
-            <a
-              href="https://jefclarem-logistics.vercel.app/"
-              className="btn btn-primary"
-              target="_blank"
-            >
-              live Demo
-            </a>
-          </div>
-        </article>
-      </div>
+      <div className="container portfolio__container"></div>
     </section>
   );
 };
