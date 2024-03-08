7c5f7ce41459014508b738ab7fc9bc01edfded98
Megagig <megagigsoftwaresolutions.com>
2024-03-08 15:52:24 +0100

    Generating Portfolio Items from Array using map method

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -57,7 +57,26 @@ const Portfolio = () => {
     <section id="portfolio">
       <h5>My Recent Work</h5>
       <h2>Portfolio</h2>
-      <div className="container portfolio__container"></div>
+      <div className="container portfolio__container">
+        {data.map(({ id, image, title, Github, demo }) => {
+          return (
+            <article key={id} className="portfolio__item">
+              <div className="portfolio__item-image">
+                <img src={image} alt={title} />
+              </div>
+              <h3>{title}</h3>
+              <div className="portfolio__item-cta">
+                <a href={Github} className="btn" target="_blank">
+                  Github
+                </a>
+                <a href={demo} className="btn btn-primary" target="_blank">
+                  live Demo
+                </a>
+              </div>
+            </article>
+          );
+        })}
+      </div>
     </section>
   );
 };
