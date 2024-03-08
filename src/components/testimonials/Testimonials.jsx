802e23a62b49a57436237ef7d9fdb8feae8fd76d
Megagig <megagigsoftwaresolutions.com>
2024-03-08 16:35:45 +0100

    Duplicate the article tag reviews so that i can style them

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -13,7 +13,54 @@ const Testimonials = () => {
       <div className="container testimonials__container">
         <article className="testimonial">
           <div className="client__avatar">
-            <img src={Abel} alt="Anthony" />
+            <img src={Abel} alt="Abel Morara" />
+            <h5 className="client__name">Abel Morara</h5>
+            <small className="client__review">
+              I had the privilege of working closely with Anthony during our
+              time together in a software development program, and I must say
+              that he is an exceptional software engineer. Anthony's meticulous
+              attention to detail and thorough approach to his work made
+              collaborating with him an absolute delight.
+            </small>
+          </div>
+        </article>
+        <article className="testimonial">
+          <div className="client__avatar">
+            <img src={Abel} alt="Abel Morara" />
+            <h5 className="client__name">Abel Morara</h5>
+            <small className="client__review">
+              I had the privilege of working closely with Anthony during our
+              time together in a software development program, and I must say
+              that he is an exceptional software engineer. Anthony's meticulous
+              attention to detail and thorough approach to his work made
+              collaborating with him an absolute delight.
+            </small>
+          </div>
+        </article>
+        <article className="testimonial">
+          <div className="client__avatar">
+            <img src={Abel} alt="Abel Morara" />
+            <h5 className="client__name">Abel Morara</h5>
+            <small className="client__review">
+              I had the privilege of working closely with Anthony during our
+              time together in a software development program, and I must say
+              that he is an exceptional software engineer. Anthony's meticulous
+              attention to detail and thorough approach to his work made
+              collaborating with him an absolute delight.
+            </small>
+          </div>
+        </article>
+        <article className="testimonial">
+          <div className="client__avatar">
+            <img src={Abel} alt="Abel Morara" />
+            <h5 className="client__name">Abel Morara</h5>
+            <small className="client__review">
+              I had the privilege of working closely with Anthony during our
+              time together in a software development program, and I must say
+              that he is an exceptional software engineer. Anthony's meticulous
+              attention to detail and thorough approach to his work made
+              collaborating with him an absolute delight.
+            </small>
           </div>
         </article>
       </div>
