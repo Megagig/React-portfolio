105175b3c2eb0097f521cf19c1d716052c9a0837
Megagig <megagigsoftwaresolutions.com>
2024-03-09 01:52:44 +0100

    Finish Footer Component JSX

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,7 +1,57 @@
 import React from 'react';
 import './footer.css';
+import { FaFacebook } from 'react-icons/fa';
+import { FaInstagram } from 'react-icons/fa';
+import { FaTwitter } from 'react-icons/fa';
+import { FaLinkedin } from 'react-icons/fa';
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer>
+      <a href="#" className="footer__logo">
+        MEGAGIG
+      </a>
+      <ul className="permalinks">
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#about">About</a>
+        </li>
+        <li>
+          <a href="#experience">Experience</a>
+        </li>
+        <li>
+          <a href="#services">Services</a>
+        </li>
+        <li>
+          <a href="#portfolio">Portfolio</a>
+        </li>
+        <li>
+          <a href="#testimonials">Testimonials</a>
+        </li>
+        <li>
+          <a href="#contact">Contact</a>
+        </li>
+      </ul>
+      <div className="footer__socials">
+        <a href="https://facebook.com">
+          <FaFacebook />
+        </a>
+        <a href="https://instagram">
+          <FaInstagram />
+        </a>
+        <a href="https://twitter">
+          <FaTwitter />
+        </a>
+        <a href="https://linkedin">
+          <FaLinkedin />
+        </a>
+      </div>
+      <div className="footer__copyright">
+        <small>&copy; 2024 MEGAGIG, All Rights Reserved</small>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
